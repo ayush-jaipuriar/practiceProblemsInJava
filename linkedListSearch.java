b2e8b6443fc78c24b5b4c1538804640c921723f5
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-12-04 02:05:22 +0530

    Implemented Linked List element search program in Java

--- linkedListSearch.java
+++ linkedListSearch.java
@@ -1,10 +1,10 @@
-import java.io.*;
 import java.util.*;
+import java.util.Scanner;
 
-public class LinkedList {
+ class LinkedList {
     Node head;
 
-    static class Node {
+    public static class Node {
         int data;
         Node next;
 
@@ -14,8 +14,7 @@ public class LinkedList {
         }
     }
 
-    public static LinkedList insert(LinkedList list, int data) {
-
+     public static LinkedList insert(LinkedList list, int data) {
         Node newNode = new Node(data);
         newNode.next = null;
 
@@ -24,42 +23,32 @@ public class LinkedList {
         }
 
         else {
-
             Node lastNode = list.head;
             while (lastNode.next != null) {
                 lastNode = lastNode.next;
             }
             lastNode.next = newNode;
+
         }
+
         return list;
     }
 
-    public static void search(LinkedList list, int searchElement) {
+      public static void search(LinkedList list, int searchElement) {
         Node currentNode = list.head;
         while (currentNode != null) {
             if (currentNode.data == searchElement) {
                 System.out.println("The element is present in the linked list ");
                 return;
             }
-            currentNode = currentNode.next;
 
         }
         System.out.println("Sorry, the element you searched for was not found in the give Linked List");
         return;
     }
 
-    public static void printList(LinkedList list) {
-        Node currentNode = list.head;
-        System.out.println("Linked List : ");
-        while (currentNode != null) {
-            System.out.println(currentNode.data);
-            currentNode = currentNode.next;
-        }
-    }
-
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-
         list = insert(list, 1);
         list = insert(list, 2);
         list = insert(list, 3);
@@ -69,11 +58,12 @@ public class LinkedList {
         list = insert(list, 7);
         list = insert(list, 8);
 
-        printList(list);
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the element to search for in the given linked list");
         int searchElement = sc.nextInt();
         search(list, searchElement);
+        sc.close();
 
     }
+
 }
